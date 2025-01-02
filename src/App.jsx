5a5253e41640ffd60f72ccaab359bcc93a4efb88
grosen-coding <rosengsr@gmail.com>
2025-01-02 17:53:34 -0500

    basic site working, added GlobalStyle and theme files, added initial loading SVG animation, and a basic fade-in hero section. There are currently a few errors in the console, but everything is working so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
+import { ThemeProvider } from "styled-components";
 import AnimatedLogo from "./components/AnimatedLogo";
 import HeroSection from "./components/HeroSection";
+import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme"; // Import your theme file
 
 function App() {
   const [showHero, setShowHero] = useState(false);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <AnimatedLogo onComplete={() => setShowHero(true)} />
       <HeroSection isVisible={showHero} />
-    </>
+    </ThemeProvider>
   );
 }
 
